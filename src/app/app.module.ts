9d386aa9fff905e97f6c92cefdab04c21e107079
Valeria Ramos <valeria.r@eyeride.io>
2024-11-05 17:39:01 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ButtonModule } from 'primeng/button';
+import { SharedModule } from "./shared/shared.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    SharedModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
